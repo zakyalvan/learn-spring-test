da153c165fc07ba15c589e46d2aff7369f1f0b74
Muhammad Zaky Alvan <zakyalvan@gmail.com>
2014-04-16 09:06:42 +0700

    Adding transfer fee policy support

---
+++ src/main/java/com/innovez/service/SgdTransferFeePolicy.java
@@ -0,0 +1,17 @@
+package com.innovez.service;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.innovez.entity.Account;
+import com.innovez.entity.Money;
+import com.innovez.service.TransferService.TransferFeePolicy;
+
+@Component
+@Qualifier("SGD")
+public class SgdTransferFeePolicy implements TransferFeePolicy {
+	@Override
+	public Money calculateFee(Account fromAccount, Account account, Money transfered) {
+		return transfered;
+	}
+}
